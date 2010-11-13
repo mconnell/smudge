badbdcca71e487d20cfeee64d353da3324c341b5
Mark Connell <mark@markconnell.co.uk>
2010-11-13 20:35:28 +0000

    generate rails app.

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.1'
+gem 'sqlite3-ruby', :require => 'sqlite3'
+gem 'haml'
+
+group :development, :test do
+  gem 'rspec'
+  gem 'rspec-rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smudge::Application.config.secret_token = '24d3901e1e89db7467f62b12a9309dfff697dce607120035d2cbc69f9cd6b81e9fc973375c2d705e31d157ddbe796f45830a3b1b3fd13f131c871eaaeeb86cfe'
